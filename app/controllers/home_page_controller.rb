3087e395616994c06c2dac2cd091a74bbfc9edcc
Morgan Smith <morgansmith0219@gmail.com>
2019-10-30 23:04:06 +0000

    trying to fix 406-not-acceptable error on heroku

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,4 +1,7 @@
 class HomePageController < ApplicationController
   def index
   end
+
+  def privacy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home_page#index'
+  get 'privacy', to: 'home_page#privacy'
 end
