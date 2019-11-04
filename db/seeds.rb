3532b168a327e66b5ea8b26f92eee1e8a42e0690
Morgan Smith <morgansmith0219@gmail.com>
2019-11-04 01:35:15 +0000

    displaying user's artwork onto the page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
+category = Category.create(category_name: 'photography')
+
 user1 = User.find_by(email: 'morgansmith0219@gmail.com')
-Artwork.create(title: 'Feel', image_url: 'nature-unsplash.jpg', price: '$30')
+Artwork.create!(title: 'Feel', image_url: 'nature-unsplash.jpg', price: '30.00', category_id: '1')
 
 
 
